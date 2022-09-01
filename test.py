20f32778c63c949afbcd4c669342e1cbc607b44d
shivampanwar-lumiq <shivam.panwar@lumiq.ai>
2022-09-01 12:20:41 +0530

    updated test file with global url

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import requests
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-# data = {'url': 'http://bit.ly/mlbookcamp-pants'}
+url_local = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+url_lambda = "https://h8brgez541.execute-api.us-east-1.amazonaws.com/testing/predict"
 data1 = {"values":[[0.1,2,0.1,3]]}
 data2 ={"values":[[5.9,3.0,5.1,2.3]]}
-result1,result2 = requests.post(url, json=data1).json(),requests.post(url, json=data2).json()
+result1,result2 = requests.post(url_lambda, json=data1).json(),requests.post(url_lambda, json=data2).json()
 print(result1,result2)
